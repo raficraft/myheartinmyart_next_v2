9597f3c066d9ca16aefc425522cc7b970300cd46
raficraft <62650579+raficraft@users.noreply.github.com>
2021-10-04 17:09:07 +0200

    ---debug

--- pages/api/post/index.jsx
+++ pages/api/post/index.jsx
@@ -10,14 +10,6 @@ import path from "path";
  */
 
 export default function handler(req, res) {
-  const filePath = path.join(
-    process.cwd(),
-    "pages/api/data/blog/",
-    "posts.json"
-  );
-  const fileData = fs.readFileSync(filePath);
-  const data = JSON.parse(fileData);
-
   if (req.method === "GET") {
     res.status(200).json({ toto: "tata" });
   } else if (req.method === "POST") {
